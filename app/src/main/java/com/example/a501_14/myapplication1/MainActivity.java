942ee1eeceb9a98da7d468d501fc58e2ae74aa70
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-15 12:35:17 +0900

    no message

--- app/src/main/java/com/example/a501_14/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/a501_14/myapplication1/MainActivity.java
@@ -8,7 +8,7 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   //protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
