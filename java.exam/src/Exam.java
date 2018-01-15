c377062cced0a0f4988602763d2591b83249a84e
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-15 12:51:47 +0900

    변수

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -2,17 +2,17 @@ public class Exam
 {
     public static void main(String[] args)
     {
-        int num1;
-        num1=40;
-
-        int num2=40;
-        int num3=num1*num2;
-        System.out.println(num1+"*"+num2+"="+num3);
-
-
-
-        //문자 출력
+       /*문자 출력
         //System.out.println("");
         //System.out.println("Seoul   10,312,545     +91,375");
+        */
+
+        char name;
+        name = 'a';
+
+        char name2 = 'a';
+
+        System.out.println(name);
+
     }
 }
