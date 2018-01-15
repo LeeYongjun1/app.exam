15e23d503f9c87b82a48868757d75bd72a4ae57a
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-15 12:36:32 +0900

    no message

---
+++ java.exam/src/Exam.java
@@ -0,0 +1,18 @@
+public class Exam
+{
+    public static void main(String[] args)
+    {
+        int num1;
+        num1=40;
+
+        int num2=40;
+        int num3=num1*num2;
+        System.out.println(num1+"*"+num2+"="+num3);
+
+
+
+        //문자 출력
+        //System.out.println("");
+        //System.out.println("Seoul   10,312,545     +91,375");
+    }
+}
