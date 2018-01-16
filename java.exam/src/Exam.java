02a5de8818700b6d53d6a966fea5be79a1570d37
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-16 12:01:23 +0900

    연산자

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -2,26 +2,18 @@ public class Exam
 {
     public static void main(String[] args)
     {
-       int num1, num2, num3;
-        num1 = 30;
-        num2 = 60;
-        num3 = 90;
-        int sum = num1+num2+num3;
-        double average = sum / 3;
+       boolean result;
+       int num1, num2;
+       num1 = 30;
+       num2 = 60;
 
-        System.out.println("합계 : +sum");
-        System.out.println("평균 : +average" );
+        result = num1 < num2;
+        result = num1 > num2;
+        result = num1 <= num2;
+        result = num1 >= num2;
+        result = num1 == num2;
+        result = num1 != num2;
 
-        //int n1=7;
-        //int n2=3;
-
-       //nt result=n1+n2;
-       //System.out.println("덧셈 결과 : " + result);
-
-       //result=n1-n2;
-        //System.out.println("뺼샘 결과 : " + result);
-        //System.out.println("곱샘 결과 : " + n1*n2);
-        //System.out.println("나눗셈 결과 : " + n1/n2);
-        //System.out.println("나머지 결과 : " + n1%n2);
+       System.out.println("결과는 : "+result);
     }
 }
