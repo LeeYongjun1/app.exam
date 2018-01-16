331eccdf52cb89ef24a3b1b8baa791742891d69a
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-16 11:40:13 +0900

    ㅇㅇ

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -2,16 +2,26 @@ public class Exam
 {
     public static void main(String[] args)
     {
-       int n1=7;
-       int n2=3;
+       int num1, num2, num3;
+        num1 = 30;
+        num2 = 60;
+        num3 = 90;
+        int sum = num1+num2+num3;
+        double average = sum / 3;
 
-       int result=n1+n2;
-       System.out.println("덧셈 결과 : " + result);
+        System.out.println("합계 : +sum");
+        System.out.println("나눗셈 : +average" );
 
-       result=n1-n2;
-        System.out.println("뺼샘 결과 : " + result);
-        System.out.println("곱샘 결과 : " + n1*n2);
-        System.out.println("나눗셈 결과 : " + n1/n2);
-        System.out.println("나머지 결과 : " + n1%n2);
+        //int n1=7;
+       //int n2=3;
+
+       //nt result=n1+n2;
+       //System.out.println("덧셈 결과 : " + result);
+
+       //result=n1-n2;
+        //System.out.println("뺼샘 결과 : " + result);
+        //System.out.println("곱샘 결과 : " + n1*n2);
+        //System.out.println("나눗셈 결과 : " + n1/n2);
+        //System.out.println("나머지 결과 : " + n1%n2);
     }
 }
