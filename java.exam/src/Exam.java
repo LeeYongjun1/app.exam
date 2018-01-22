1651d7f308260b816c043c8c39f36275a901d7ee
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-22 12:50:03 +0900

    sum

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -1,19 +1,26 @@
 public class Exam {
     public static void main(String[] args) {
-       int n=3;
-       String str = "";
 
-       switch (str){
-           case "쥐":
-               System.out.println("mouse");
-                 break;
-           case "고양이":
-               System.out.println("cat");
-                 break;
-           case "강아지":
-               System.out.println("dog");
-                 break;
-       }
-          System.out.println("단어가 없습니다");
+        int Num = 0;
+        int sum = 0;
+
+        while (Num < 10) {
+            Num = Num + 1;
+            sum = sum + Num;
+        }
+
+        System.out.println("1에서 10까지 더한 값은? 55");
+
     }
 }
+
+     /*int  Num = 0;
+      while (Num < 100){
+          System.out.println("안녕하세요");
+          Num = Num +1;
+      }*/
+        /*while (Num < 100){
+            System.out.println("1+2+3+4+5+6+7+8+9+10");
+            Num = Num +1;
+        }*/
+
