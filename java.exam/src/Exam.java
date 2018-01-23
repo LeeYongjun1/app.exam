770bb5fac7a05902705a32ab85406087918ba4a6
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-23 11:56:07 +0900

    ㅇㅇㅇㅇㅇㅇㅇ22233111

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -3,17 +3,9 @@ import java.util.Scanner;
 public class Exam {
     public static void main(String[] args) {
 
-        int num=0;
-        int num2=0;
-        // do while을 활용한 반복 출력
-        do{
-            System.out.println("java" +num);
-            num++;
-        }while (num<5);
-        // while을 활용한 반복 출력
-        while(num>5){
-            System.out.println("java" +num2);
-            num2++;
-        }
+       for(int num=0; num<5; num++)
+       {
+           System.out.println("...");
+       }
     }
 }
