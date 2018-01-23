f95a5b45be239b6073a8a2662a05f5dd5b2c43d8
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-23 09:53:28 +0900

    숫자입력

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -1,26 +1,16 @@
+import java.util.Scanner;
+
 public class Exam {
     public static void main(String[] args) {
+        // 키보드 입력을 준비
+        Scanner kd = new Scanner(System.in);
+        // 입력할 숫자를 저장하는 변수
+        int num;
 
-        int Num = 1;
+        // 사용자가 입력할 숫자를 변수 num에 저장
+        num = kd.nextInt();
 
-
-        while (Num < 9) {
-            Num = 0+1;
-
-        }
-
-        System.out.println("검색종료");
-
+        // 저장한 숫자를 출력함
+        System.out.println(num);
     }
 }
-
-     /*int  Num = 0;
-      while (Num < 100){
-          System.out.println("안녕하세요");
-          Num = Num +1;
-      }*/
-        /*while (Num < 100){
-            System.out.println("1+2+3+4+5+6+7+8+9+10");
-            Num = Num +1;
-        }*/
-
