e481635b38cae30633cc2daf5a573cb559ceb314
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-30 12:10:41 +0900

    adder

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -1,19 +1,87 @@
 import java.util.Scanner;
 
 public class Exam {
-    public static void main(String[] args)
-    {
-       int result=adder(4, 5);
-       System.out.println("4와 5의 합 : " + result );
-       System.out.println("3.5의 제곱 : " + square(3.5));
+    public static void main(String[] args) {
+        // 사용자가 입력한 숫자를 저장하는 변수
+        double num1;
+        double num2;
+
+        // 연산자를 구분할 문자를 저장하는 변수
+        String operator;
+
+        // 사용자가 키보드 입력 준비
+        Scanner kd = new Scanner(System.in);
+        // 사용자로 부터 숫자 입력
+        System.out.println("첫번째 숫자를 입력하세요.");
+        num1 = kd.nextDouble();
+        // 개행문자 제거를 위한 flsh 처리
+       kd.nextLine();
+
+        System.out.println("연산을 선택하세요(덧셈:a, 뱰셈:b, 곱셈:c,나눗셈:b, 나머지:e)");
+        operator = kd.nextLine();
+
+        System.out.println("두번쨰 숫자를 입력하세요.");
+        num2 = kd.nextDouble();
+
+        switch (operator) {
+            case "a":
+                System.out.println("덧샘한 결과는 다음과 같습니다.");
+                System.out.println(adder(num1, num2));
+                break;
+            case "b":
+                System.out.println("뺼샘한 결과는 다음과 같습니다.");
+                System.out.println(minus(num1, num2));
+                break;
+            case "c":
+                System.out.println("곱샘한 결과는 다음과 같습니다.");
+                System.out.println(multi(num1, num2));
+                break;
+            case "d":
+                System.out.println("나눗셈 결과는 다음과 같습니다.");
+                System.out.println(divide(num1, num2));
+                break;
+            case "e":
+                System.out.println("나머지한 결과는 다음과 같습니다.");
+                System.out.println(modular(num1, num2));
+                break;
+            default:
+                System.out.println("연산을 잘못 입력하였습니다.");
+        }
+        System.out.println(adder(num1, num2));
+        System.out.println(minus(num1, num2));
+        System.out.println(multi(num1, num2));
+        System.out.println(divide(num1, num2));
+        System.out.println(modular(num1, num2));
     }
-    public static int adder(int num1, int num2)
-    {
-      int addResult=num1+num2;
-      return addResult;
+    public static double adder(double num1, double num2) {
+        // 덧셉값을 저장하는 변수 생성
+        double result;
+        // 덧셋 연산 실행
+        result = num1 + num2;
+        // 덧셈한 결과 값을 리턴(도출)
+        return result;
     }
-    public static double square(double num)
-    {
-        return  num*num;
+    public static double minus(double num1, double num2) {
+        // 곱셈값을 저장하는 변수 생성
+        double result;
+        // 곱셉 연산 실행
+        result = num1 - num2;
+        // 곱셋한 결과를 리턴(도출)
+        return result;
+    }
+    public static double multi(double num1, double num2) {
+        double result;
+        result = num1 * num2;
+        return result;
+    }
+    public static double divide(double num1, double num2) {
+        double result;
+        result = num1 / num2;
+        return result;
+    }
+    public static double modular(double num1, double nm2){
+        double result;
+        result = num1 % nm2;
+        return result;
     }
 }
