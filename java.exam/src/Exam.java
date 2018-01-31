d256b7d2badcd3f303f1a4711997999684a84db0
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-31 10:50:10 +0900

    ㅇㅇㅈㅇㅈㅇ

--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -1,37 +1,66 @@
+import java.util.Scanner;
 
-public class Exam
- {
-     public static void main(String[] args)
-     {
-       int n1=7;
-       int n2=3;
-       int num1, num2, num3;
-        num1 = 30;
-        num2 = 60;
-        num3 = 90;
-        int sum = num1+num2+num3;
-        double average = sum / 3;
- 
-       int result=n1+n2;
-       System.out.println("덧셈 결과 : " + result);
-        System.out.println("합계 : +sum");
-        System.out.println("나눗셈 : +average" );
+public class Exam {
+    public static void main(String[] args) {
+        // 사용자가 입력한 숫자를 저장하는 변수
+        int num1;
+        // 사용자 키보드 입력 준비
+        Scanner kd = new Scanner(System.in);
+        // 사용자로 부터 숫자 입력
+        System.out.println("반지름을 입력하세요.");
+        num1 = kd.nextInt();
 
-       result=n1-n2;
-        System.out.println("뺼샘 결과 : " + result);
-        System.out.println("곱샘 결과 : " + n1*n2);
-        System.out.println("나눗셈 결과 : " + n1/n2);
-        System.out.println("나머지 결과 : " + n1%n2);
-        //int n1=7;
-       //int n2=3;
-
-       //nt result=n1+n2;
-       //System.out.println("덧셈 결과 : " + result);
-
-       //result=n1-n2;
-        //System.out.println("뺼샘 결과 : " + result);
-        //System.out.println("곱샘 결과 : " + n1*n2);
-        //System.out.println("나눗셈 결과 : " + n1/n2);
-        //System.out.println("나머지 결과 : " + n1%n2);
+        System.out.println("원의 넓이 는 ?");
+        System.out.println(area(num1));
     }
- }
+    public static double area(int radian){
+        double temp;
+        double result;
+
+        temp = multi(radian, radian);
+        result = multi(temp, 3.14);
+        // result = radian * radian *3.14;
+        return result;
+    }
+    public static double multi(double num1, double num2){
+        double result;
+        // 곱셈 연산 실행
+        result = num1 * num2;
+       // 곱셈한 결과 값을 기턴(도출)
+        return result;
+    }
+    public static double  adder(double num1, double num2){
+        double result;
+
+        result = num1 + num2;
+
+        return  result;
+    }
+    public static double minus(double num1, double num2){
+        double result;
+
+        result = num1 - num2;
+
+        return result;
+    }
+    public static double divide(double num1, double num2){
+        double result;
+
+        result = num1 / num2;
+
+        return result;
+    }
+    public static double modular(double num1, double num2){
+        double result;
+
+        result = num1 % num2;
+
+        return result;
+    }
+        /* System.out.println(adder(num1, num2)); 더하기
+           System.out.println(minus(num1, num2)); 뺴기
+           System.out.println(multi(num1, num2)); 곱하기
+           System.out.println(divide(num1,num2)); 나눗셈
+          System.out.println(modular(num1, num2)); 나머지 */
+
+}
