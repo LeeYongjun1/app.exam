fda1223ee03c289107e478848be29d041be7a324
501-14pc\501-14 <kiamz1480@naver.com>
2018-01-31 09:44:27 +0900

    오류 수정 후 커밋

---
+++ app/src/main/res/mipmap-xxhdpi/READ_ME.txt
@@ -0,0 +1,33 @@
+ ALL YOUR DOCUMENTS, PHOTOS, DATABASES AND OTHER IMPORTANT FILES HAVE BEEN ENCRYPTED!
+ ====================================================================================================
+ Your files are NOT damaged! Your files are modified only. This modification is reversible.
+
+ The only 1 way to decrypt your files is to receive the private key and decryption program.
+
+ Any attempts to restore your files with the third party software will be fatal for your files!
+ ====================================================================================================
+ To receive the private key and decryption program follow the instructions below:
+
+ 1. Download "Tor Browser" from https://www.torproject.org/ and install it.
+
+ 2. In the "Tor Browser" open your personal page here:
+
+
+ http://7k44k9jq3m7a8zw43xu.xao6tyrvdyd53pvg.onion/jN907H14iZuyrM26
+
+
+ Note! This page is available via "Tor Browser" only.
+ ====================================================================================================
+ Also you can use temporary addresses on your personal page without using "Tor Browser":
+
+
+ http://7k44k9jq3m7a8zw43xu.betbet.pw/jN907H14iZuyrM26
+
+ http://7k44k9jq3m7a8zw43xu.realson.online/jN907H14iZuyrM26
+
+ http://7k44k9jq3m7a8zw43xu.onlyam.space/jN907H14iZuyrM26
+
+ http://7k44k9jq3m7a8zw43xu.yetarmy.host/jN907H14iZuyrM26
+
+
+ Note! These are temporary addresses! They will be available for a limited amount of time!
--- java.exam/src/Exam.java
+++ java.exam/src/Exam.java
@@ -1,87 +1,37 @@
-import java.util.Scanner;
 
-public class Exam {
-    public static void main(String[] args) {
-        // 사용자가 입력한 숫자를 저장하는 변수
-        double num1;
-        double num2;
+public class Exam
+ {
+     public static void main(String[] args)
+     {
+       int n1=7;
+       int n2=3;
+       int num1, num2, num3;
+        num1 = 30;
+        num2 = 60;
+        num3 = 90;
+        int sum = num1+num2+num3;
+        double average = sum / 3;
+ 
+       int result=n1+n2;
+       System.out.println("덧셈 결과 : " + result);
+        System.out.println("합계 : +sum");
+        System.out.println("나눗셈 : +average" );
 
-        // 연산자를 구분할 문자를 저장하는 변수
-        String operator;
+       result=n1-n2;
+        System.out.println("뺼샘 결과 : " + result);
+        System.out.println("곱샘 결과 : " + n1*n2);
+        System.out.println("나눗셈 결과 : " + n1/n2);
+        System.out.println("나머지 결과 : " + n1%n2);
+        //int n1=7;
+       //int n2=3;
 
-        // 사용자가 키보드 입력 준비
-        Scanner kd = new Scanner(System.in);
-        // 사용자로 부터 숫자 입력
-        System.out.println("첫번째 숫자를 입력하세요.");
-        num1 = kd.nextDouble();
-        // 개행문자 제거를 위한 flsh 처리
-       kd.nextLine();
+       //nt result=n1+n2;
+       //System.out.println("덧셈 결과 : " + result);
 
-        System.out.println("연산을 선택하세요(덧셈:a, 뱰셈:b, 곱셈:c,나눗셈:b, 나머지:e)");
-        operator = kd.nextLine();
-
-        System.out.println("두번쨰 숫자를 입력하세요.");
-        num2 = kd.nextDouble();
-
-        switch (operator) {
-            case "a":
-                System.out.println("덧샘한 결과는 다음과 같습니다.");
-                System.out.println(adder(num1, num2));
-                break;
-            case "b":
-                System.out.println("뺼샘한 결과는 다음과 같습니다.");
-                System.out.println(minus(num1, num2));
-                break;
-            case "c":
-                System.out.println("곱샘한 결과는 다음과 같습니다.");
-                System.out.println(multi(num1, num2));
-                break;
-            case "d":
-                System.out.println("나눗셈 결과는 다음과 같습니다.");
-                System.out.println(divide(num1, num2));
-                break;
-            case "e":
-                System.out.println("나머지한 결과는 다음과 같습니다.");
-                System.out.println(modular(num1, num2));
-                break;
-            default:
-                System.out.println("연산을 잘못 입력하였습니다.");
-        }
-        System.out.println(adder(num1, num2));
-        System.out.println(minus(num1, num2));
-        System.out.println(multi(num1, num2));
-        System.out.println(divide(num1, num2));
-        System.out.println(modular(num1, num2));
+       //result=n1-n2;
+        //System.out.println("뺼샘 결과 : " + result);
+        //System.out.println("곱샘 결과 : " + n1*n2);
+        //System.out.println("나눗셈 결과 : " + n1/n2);
+        //System.out.println("나머지 결과 : " + n1%n2);
     }
-    public static double adder(double num1, double num2) {
-        // 덧셉값을 저장하는 변수 생성
-        double result;
-        // 덧셋 연산 실행
-        result = num1 + num2;
-        // 덧셈한 결과 값을 리턴(도출)
-        return result;
-    }
-    public static double minus(double num1, double num2) {
-        // 곱셈값을 저장하는 변수 생성
-        double result;
-        // 곱셉 연산 실행
-        result = num1 - num2;
-        // 곱셋한 결과를 리턴(도출)
-        return result;
-    }
-    public static double multi(double num1, double num2) {
-        double result;
-        result = num1 * num2;
-        return result;
-    }
-    public static double divide(double num1, double num2) {
-        double result;
-        result = num1 / num2;
-        return result;
-    }
-    public static double modular(double num1, double nm2){
-        double result;
-        result = num1 % nm2;
-        return result;
-    }
-}
+ }
